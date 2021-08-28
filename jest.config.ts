0911048476c8c174091d69aac893a26ce06c664b
Natália Fonseca <nataliabf@outlook.com>
2021-08-28 15:13:54 -0300

    chore: add main dependencies and config files

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  preset: "ts-jest",
+  testMatch: ["**/*.spec.ts"],
+  bail: true,
+};
